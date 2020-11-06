c87521165423b99643f54b6a4b98b9f83fd4834e
alexAdri28 <rio_kevin1999@hotmail.com>
2020-11-06 20:15:30 +0000

    update  module

---
+++ app03-studentGrades/module.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class module here.
+ *
+ * @author (Alex Adriano)
+ * @version (05/11/2020)
+ */
+public class module
+{
+    private String title;
+    
+    private String codeNo;
+    
+    private int mark;
+    /**
+     * Constructor for objects of class module
+     *
+     */
+    
+    public module()
+    {
+        
+    }
+    public void print()
+    { 
+        System.out.println("Module: " + codeNo +
+        " "+ title +"Mark= " +mark);
+        
+    }
+}
+
+
+    
