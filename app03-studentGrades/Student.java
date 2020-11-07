2e492edc9754c4a51b8ec24a6c43a83c9c504762
alexAdri28 <rio_kevin1999@hotmail.com>
2020-11-07 20:06:05 +0000

    this is the molude update

--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -15,6 +15,7 @@ public class Student
     private String id;
     // the amount of credits for study taken so far
     private int credits;
+    private Course course;
     
     /**
      * Create a new student with a given name and ID number.
@@ -84,4 +85,13 @@ public class Student
     {
         System.out.println(name + ", student ID: " + id + ", credits: " + credits);
     }
+    public void enrolOnCourse(Course course)
+    {
+        this.course = course;
+    }
+   
+    
+    
+   
+    
 }
--- app03-studentGrades/module.java
+++ app03-studentGrades/module.java
@@ -1,33 +1,31 @@
 
 /**
- * Write a description of class module here.
+ * This Class is called module where i have added the Mark, title and Code number for each module that Im going to be doing in
+ * semester One.
  *
- * @author (Alex Adriano)
- * @version (05/11/2020)
+ * @author (Alex  Gordillo Adriano)
+ * @version (06/11/2020)
  */
-public class module
+public class Module
 {
     private String title;
     
     private String codeNo;
     
     private int mark;
-    /**
-     * Constructor for objects of class module
-     *
-     */
     
-    public module()
-    {
-        
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String title, String codeNo)
+    { 
+        mark = 0;
+        this.title =title;
+        this.codeNo=codeNo;
     }
     public void print()
     { 
-        System.out.println("Module: " + codeNo +
-        " "+ title +"Mark= " +mark);
-        
+        System.out.println("Module:" + codeNo + " " + "Mark=" + mark);
     }
 }
-
-
-    
